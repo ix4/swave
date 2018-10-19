d54a7d335133541de294e617619d14aaa6074030
lauraalpinto <lauripri@gmail.com>
2018-10-19 15:39:09 +0100

    Store + Contact Page

--- js/index.js
+++ js/index.js
@@ -1,6 +1,5 @@
 //UI
 var viewCtrl = (function (){
-
   var homeElements = {
     sections: [
       sectionAbout = [document.querySelector('.about'), '.about'],
@@ -29,9 +28,38 @@ var viewCtrl = (function (){
       section4 = [document.querySelector('.s1__options-3'), '.s-3'],
     ]
   }
-  var storeElements = {}
-  var propertiesElements = {}
-  var contactElements = {}
+  var storeElements = {
+    sections : [
+      section1 = [document.querySelector('.s1__content'), '.s-1'],
+      section2 = [document.querySelector('.s1__map'), '.s-1'],
+    ],
+    map : document.getElementById('map')
+  }
+
+  var propertiesElements = {
+    sections: [
+      section1 = [document.querySelector('.s1__content'), '.s-c'],
+      section2 = [document.querySelector('.s1__properties'), '.s-1']
+    ],
+    closeBtn : [document.querySelector('.popup__close')],
+    properties : [document.querySelector('.s1__properties')],
+    property : [document.querySelectorAll('.property')],
+    popup : [document.querySelector('.popup')],
+    popupImg : document.querySelector('.popup__image'),
+    popupTitle : document.querySelector('.popup__title'),
+    popupDesc : document.querySelector('.popup__description'),
+    popupDistance: document.querySelector('.feature__distance'),
+    popupAdults: document.querySelector('.feature__adults'),
+    popupPrice: document.querySelector('.feature__price'),
+    popupArea: document.querySelector('.feature__area')
+  }
+
+  var contactElements = {
+    sections: [
+      section1 = [document.querySelector('.contacts'), '.s-c']
+    ],
+    map : document.getElementById('map')
+  }
 
   var elements = {
 
@@ -93,7 +121,6 @@ var viewCtrl = (function (){
 
     changeOpacity : function(className) {
       document.querySelectorAll(className).forEach(el => {
-        console.log(el);
         el.style.opacity = 1;
         el.style.transform = 'translateX(0)';
       })
@@ -106,8 +133,6 @@ var viewCtrl = (function (){
         elements.navBar.classList.remove('changeNav');
       }
     },
-
-
 
     menuStateToggle : function(menuState) {
       if (menuState){
@@ -123,6 +148,17 @@ var viewCtrl = (function (){
       }
 
     },
+
+    updatePopup : function(info){
+      propertiesElements.popupTitle.innerHTML = info.name;
+      propertiesElements.popupDesc.innerHTML = info.description;
+      propertiesElements.popupImg.style.backgroundImage = `url(${info.photo})`;
+      propertiesElements.popupDistance.innerHTML = info.distance;
+      propertiesElements.popupAdults.innerHTML = info.adults;
+      propertiesElements.popupPrice.innerHTML = info.price;
+      propertiesElements.popupArea.innerHTML = info.area;
+    },
+
 
     elementPos: function(element, pos, number) {
       if (pos === 'top'){
@@ -142,21 +178,106 @@ var viewCtrl = (function (){
 // MODEL
 var modelCtrl = (function (){
 
-  var GetZoomFactor = function() {
-    var factor = 1;
-    if (document.body.getBoundingClientRect) {
-      // rect is only in physical pixel size in IE before version 8
-      var rect = document.body.getBoundingClientRect ();
-      var physicalW = rect.right - rect.left;
-      var logicalW = document.body.offsetWidth;
-
-      // the zoom level is always an integer percent value
-      factor = Math.round ((physicalW / logicalW) * 100) / 100;
+  class House {
+    constructor(name, description, photo, distance, adults, price, area){
+      this.name = name;
+      this.description = description;
+      this.photo = photo;
+      this.distance = distance;
+      this.adults = adults;
+      this.price = price;
+      this.area = area;
     }
-    return factor;
-  }
+  };
 
-  return {
+  var house1 = new House('The Cozy Cottage',
+  'A beautiful house by the beach where you can breath in fresh air and listen to the waves.',
+  '../../img/house1.jpg',
+  '500m',
+  '3',
+  '50€/night',
+  '450m'
+);
+
+var house2 = new House('The Cozy Cottage',
+  ' A beautiful house by the beach where you can breath in fresh air and listen to the waves.',
+  '../../img/house2.jpg',
+  '500m',
+  '3',
+  '50€/night',
+  '450m'
+);
+
+var house3 = new House('The Cozy Cottage',
+  'A beautiful house by the beach where you can breath in fresh air and listen to the waves.',
+  '../../img/house3.jpg',
+  '500m',
+  '3',
+  '50€/night',
+  '450m'
+);
+
+var house4 = new House('The Cozy Cottage',
+  'A beautiful house by the beach where you can breath in fresh air and listen to the waves.',
+  '../../img/house4.jpg',
+  '500m',
+  '3',
+  '50€/night',
+  '450m'
+);
+
+var house5 = new House('The Cozy Cottage',
+  'A beautiful house by the beach where you can breath in fresh air and listen to the waves.',
+  '../../img/house5.jpg',
+  '500m',
+  '3',
+  '50€/night',
+  '450m'
+);
+
+var house6 = new House('The Cozy Cottage',
+  'A beautiful house by the beach where you can breath in fresh air and listen to the waves.',
+  '../../img/header_propertie.jpg',
+  '500m',
+  '3',
+  '50€/night',
+  '450m'
+);
+
+
+var GetZoomFactor = function() {
+  var factor = 1;
+  if (document.body.getBoundingClientRect) {
+    // rect is only in physical pixel size in IE before version 8
+    var rect = document.body.getBoundingClientRect ();
+    var physicalW = rect.right - rect.left;
+    var logicalW = document.body.offsetWidth;
+
+    // the zoom level is always an integer percent value
+    factor = Math.round ((physicalW / logicalW) * 100) / 100;
+  }
+  return factor;
+}
+
+return {
+
+
+
+    getHouse : function (number){
+      if (number == 1) {
+        return house1;
+      } else if (number == 2){
+        return house2;
+      } else if (number ==3){
+        return house3;
+      } else if (number ==4){
+        return house4;
+      } else if (number ==5){
+        return house5;
+      } else if (number ==6){
+        return house6;
+      }
+    },
 
     checkIfScroll: function(e, ticking){
       return ticking;
@@ -250,7 +371,40 @@ var appCtrl = (function (){
   var ticking = false;
   var menuState = false;
 
+  var initMap = function(){
+    //Location
+    var titahi = {lat: -41.105099 , lng: 174.836385 };
 
+    //Create map
+    var map = new google.maps.Map(
+      document.getElementById('map'), {zoom: 17, center: titahi});
+
+      //Marker
+      var marker = new google.maps.Marker({position: titahi, map: map});
+    };
+
+  var onTogglePopup = function(number){
+    //1. Get Elements
+    var domEl = viewCtrl.getPath();
+
+    if (number && typeof(number) === 'string'){
+      console.log('ay');
+      console.log(number);
+      //2. Get number
+      var num = number.replace(/\D/g, "");
+
+      //3. Get Information
+      var info = modelCtrl.getHouse(num);
+      console.log(info);
+
+      //4. Write Information on UI
+      viewCtrl.updatePopup(info);
+    }
+
+    //3. Show/Hide Popup
+    domEl.popup[0].classList.toggle('popup-target');
+
+  };
 
   var toggleScroll = function(){
     if (menuState == true){
@@ -268,7 +422,6 @@ var appCtrl = (function (){
 
       // 2. Check if scroll has reached
       if (secPos <= val) {
-        console.log(cur);
         //2.1 If reached, change opacity
         viewCtrl.changeOpacity(cur[1], 1);
 
@@ -277,7 +430,7 @@ var appCtrl = (function (){
           domEl.wavepic1.style.transform = 'translateY(0)';
           domEl.wavepic2.style.transform = 'translateY(-7rem)';
           domEl.wavepic3.style.transform = 'translateY(3rem)';
-        } else if (cur[1] == '.st' || cur[1] == '.s-2') {
+        } else if (cur[1] == '.st' || cur[1] == '.s-1') {
           viewCtrl.elementOpacity(DOM.footer, 1);
         }
       }
@@ -321,8 +474,10 @@ var appCtrl = (function (){
     toggleScroll();
   };
 
-  var setupEventListeners = function(DOM){
+  var setupEventListeners = function(){
     var DOM = viewCtrl.getElements();
+    var domEl = viewCtrl.getPath();
+    //console.log(domEl.closeBtn[0]);
 
     //1. On Page Load
     document.addEventListener("DOMContentLoaded", pageLoaded);
@@ -330,6 +485,24 @@ var appCtrl = (function (){
     window.addEventListener('scroll', scrollAct);
     // 3. Check if Menu got Clicked;
     DOM.menuFive.addEventListener('click', onClickMenu);
+
+    // 4. Properties Clicked
+    if (domEl.properties){
+      domEl.properties[0].addEventListener('click', e => {
+        const btn = e.target.closest('.property');
+        if (btn) {
+          onTogglePopup(e.target.classList[1]);
+        }
+      });
+
+      domEl.popup[0].addEventListener('click', e => {
+        if (e.target.id == 'popup') {
+          onTogglePopup();
+        }
+      })
+
+      domEl.closeBtn[0].addEventListener('click', onTogglePopup);
+    }
     // 4. Avoid Scroll
     window.addEventListener('scroll', noscroll);
   };
@@ -339,6 +512,7 @@ var appCtrl = (function (){
   };
 
   var pageLoaded = function(){
+    var domEl = viewCtrl.getPath();
     var DOM = viewCtrl.getElements();
     // 1. Allow Scroll
     window.removeEventListener('scroll',noscroll);
@@ -347,7 +521,14 @@ var appCtrl = (function (){
     DOM.loader.classList.toggle('display-none');
 
     // 3. Slide Heading
+    if (DOM.headingTitle){
     DOM.headingTitle.classList.toggle('slide-left');
+  }
+
+    //5. Load map
+    if (domEl.map){
+      initMap();
+    }
   };
 
   return {
@@ -358,11 +539,16 @@ var appCtrl = (function (){
 
       // 2. Start Events
       setupEventListeners();
+
       // 3. Hide footer
+      if(DOM.footer){
       viewCtrl.elementOpacity(DOM.footer, 0);
+      }
 
       // 4. Wave of Loader
       modelCtrl.buildWave(90, 60, DOM);
+
+
     }
 
   }
